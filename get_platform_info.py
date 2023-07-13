59b4e9e587a8e43520e2703a0307621cf2c10f94
Chertanov <yaroslav.chertanov@nure.ua>
2023-07-13 20:31:52 +0300

    first_commit

---
+++ get_platform_info.py
@@ -0,0 +1,14 @@
+import platform
+
+
+def platform_info():
+    print(f"Operating system: {platform.system()}")
+    print(f"OS version: {platform.version()}")
+    print(f"Processor name: {platform.processor()}")
+
+def main():
+    platform_info()
+
+
+if __name__ == "__main__":
+    main()
---
+++ main.py
@@ -0,0 +1,9 @@
+from get_platform_info import platform_info
+
+
+def main():
+    platform_info()
+
+
+if __name__ == "__main__":
+    main()
