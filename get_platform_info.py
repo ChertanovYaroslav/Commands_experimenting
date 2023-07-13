d160ae99e8405eeffed6604fa1a8b0a5ead53596
ChertanovYaroslav <chertanovyaraslav@gmail.com>
2023-07-13 21:49:44 +0300

    added architecture output

--- get_platform_info.py
+++ get_platform_info.py
@@ -3,6 +3,7 @@ import platform
 
 def platform_info():
     print(f"Operating system: {platform.system()}")
+    print(f"Architecture: {platform.machine()}")
     print(f"OS version: {platform.version()}")
     print(f"Processor name: {platform.processor()}")
 
