e5bd01d8184168ec05d4933dd377c17e717855e8
ChertanovYaroslav <chertanovyaraslav@gmail.com>
2023-07-14 18:39:18 +0300

    added PC working time information

--- get_current_session_info.py
+++ get_current_session_info.py
@@ -1,9 +1,11 @@
-from get_platform_info import platform_info as platform
-from get_current_session_info import get_session_info
+import psutil
+
+
+def get_session_info():
+    return psutil.cpu_times(), 1
 
 
 def main():
-    platform()
     print("User time:",get_session_info()[0][0])
     print("System time:",get_session_info()[0][1])
     print("Idle time:",get_session_info()[0][2])
